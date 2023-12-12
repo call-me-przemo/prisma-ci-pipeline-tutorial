3a0ed1acce2888bae929972e87a29d41ef16087d
Przemysław Mielniczek <pmiel13@gmail.com>
2023-12-12 21:21:06 +0100

    Reset to main

--- scripts/db-startup.sh
+++ scripts/db-startup.sh
@@ -2,7 +2,7 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
-docker-compose up -d
+docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
